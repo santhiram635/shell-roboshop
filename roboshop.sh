5ad8beb517d70cd08cbcc1b1f5f2b41a7488085d
prasanthi <santhi@ram.com>
2025-10-10 17:31:32 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 
+# Define your AMI and Security Group ID
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-020cf2e239d3ab3ae"
 
-for instance in $@
+# Loop over all arguments passed to the script
+for instance in "$@"
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro 
-    --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,
-    Tags=[{Key=Name,Value=Test}]' --query 'Instances[0].InstanceId' --output $instance)
-    # this it will create the instance id
+    echo "Launching instance: $instance"
     
-    # get private ip
-    if [ $instance !="frontend" ]; then
-       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 
-       'Reservations[0].Instances[0].PrivateIpAddress' --output $instance)
+    # Launch EC2 instance and capture the instance ID
+    INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id "$AMI_ID" \
+        --instance-type t3.micro \
+        --security-group-ids "$SG_ID" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text)
+
+    echo "Launched Instance ID: $INSTANCE_ID"
+
+    # Wait for the instance to be in 'running' state
+    echo "Waiting for instance to start..."
+    aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
+
+    # Get IP address (Private or Public based on instance name)
+    if [ "$instance" != "frontend" ]; then
+        IP=$(aws ec2 describe-instances \
+            --instance-ids "$INSTANCE_ID" \
+            --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+            --output text)
     else
-       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 
-       'Reservations[0].Instances[0].PublicIpAddress' --output $instance)
+        IP=$(aws ec2 describe-instances \
+            --instance-ids "$INSTANCE_ID" \
+            --query 'Reservations[0].Instances[0].PublicIpAddress' \
+            --output text)
     fi
-      echo "$instance:$IP"
-done        
+
+    # Output instance name and its IP
+    echo "$instance: $IP"
+done      
