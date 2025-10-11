85736fe568fda781edfa7f7ef321fd3c2b2d4785
prasanthi <santhi@ram.com>
2025-10-11 19:14:10 +0530

    trap

---
+++ catalogue-set.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -euo pipefail
+
+trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.mokshi.fun
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+
+dnf module disable nodejs -y &>>$LOG_FILE
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+
+dnf install nodejs -y &>>$LOG_FILE
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+
+mkdir /app
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+
+cd /app
+
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+
+npm install &>>$LOG_FILE
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+
+systemctl daemonaaa-reload &>>$LOG_FILE
+
+systemctl enable catalogue &>>$LOG_FILE
+systemctl start catalogue
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+
+mongosh --host $MONGODB_HOST </app/db/master-data.js
